a7770c17d426737ed287f65229f86a6aa890b8f3
Eliza Salles <elizamsalles@gmail.com>
2023-03-24 18:36:16 +0000

    add background img

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -3,44 +3,52 @@ import styles from "./Form.module.css";
 
 export function Form() {
   return (
-    <div className={styles.container}>
-      <div className={styles.header}>
-        <h1>Acesse a plataforma</h1>
-        <p>
-          Faça login ou registre-se para começar a construir seus projetos ainda
-          hoje.
-        </p>
-      </div>
 
-      <div className={styles.form}>
-        <form>
-          <div className={styles.email}>
-            <label>E-mail</label>
-          </div>
-          <div>
-            <input type="email" name="email" placeholder="Digite seu e-mail" />
-          </div>
+    <div className={styles.page}>
 
-          <div className={styles.password}>
-            <label>Senha</label>
-            <a href="https://#">Esqueceu a senha?</a>
-          </div>
-          <div /*className={styles.inputWrapper}*/>
-            <input type="password" placeholder="Digite sua senha" />
-          </div>
-          {/* <div className={styles.icon}>
-                        <EyeSlash size={20} /> <Eye size={20}/>
-                    </div>  */}
 
-          <button type="submit">Entrar</button>
-        </form>
-      </div>
+        <div className={styles.container}>
+            <div className={styles.header}>
+                <h1>Acesse a plataforma</h1>
+                <p>
+                Faça login ou registre-se para começar a construir seus projetos ainda
+                hoje.
+                </p>
+            </div>
 
-      <div className={styles.footer}>
-        <footer>
-            <p>Ainda não tem uma conta? <a href="https://#">Inscreva-se</a></p>      
-        </footer>
-      </div>
+            <div className={styles.form}>
+                <form>
+                    <div className={styles.email}>
+                        <label>E-mail</label>
+                    </div>
+                    <div>
+                        <input type="email" name="email" placeholder="Digite seu e-mail" />
+                    </div>
+
+                    <div className={styles.password}>
+                        <label>Senha</label>
+                        <a href="https://#">Esqueceu a senha?</a>
+                    </div>
+                    <div /*className={styles.inputWrapper}*/>
+                        <input type="password" placeholder="Digite sua senha" />
+                    </div>
+                    {/* <div className={styles.icon}>
+                                    <EyeSlash size={20} /> <Eye size={20}/>
+                                </div>  */}
+
+                    <button type="submit">Entrar</button>
+                </form>
+            </div>
+
+            <div className={styles.footer}>
+                <footer>
+                    <p>Ainda não tem uma conta? <a href="https://#">Inscreva-se</a></p>      
+                </footer>
+            </div>
+        </div>
+
+        <img className={styles.bg} src="src\assets\bg.jpg" alt="imagem de background"/>
+
     </div>
   );
 }
